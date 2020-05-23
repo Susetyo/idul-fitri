a11620f35bbb7d9fd2721eb20c188614fe440bd5
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2020-05-24 01:44:50 +0700

    idul fitri

--- src/App.js
+++ src/App.js
@@ -9,21 +9,20 @@ function App() {
             <div className="container">
                 <div className="content">
                     <img src="https://cdn.discordapp.com/attachments/694815757665566741/713804082636390508/islam.png" />
-                    <Typing>
-                        <div className="quote-one">
-                            The noblest man is a human who can forgive the
-                            faults of others.
-                        </div>
-                        <div className="quote-two">
-                            The day of this glorious, I apologize for any
-                            mistakes.
-                        </div>
-                        <div className="quote-three">
-                            Purify our hearts purify ourselves.
-                        </div>
-                        <div className="quote-four">"Happy Eid Mubarak"</div>
-                        <div className="family">~Susetyo and Family</div>
-                    </Typing>
+                    {/* <Typing> */}
+                    <div className="quote-one">
+                        The noblest man is a human who can forgive the faults of
+                        others.
+                    </div>
+                    <div className="quote-two">
+                        The day of this glorious, I apologize for any mistakes.
+                    </div>
+                    <div className="quote-three">
+                        Purify our hearts purify ourselves.
+                    </div>
+                    <div className="quote-four">"Happy Eid Mubarak"</div>
+                    <div className="family">~Susetyo and Family</div>
+                    {/* </Typing> */}
                 </div>
             </div>
         </div>
