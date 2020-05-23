fb8029ee8bb4f525ed14c080661c096794dd62fd
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2020-05-24 01:19:45 +0700

    idul fitri

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Typing from "react-typing-animation";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container-img">
+            <div className="container">
+                <div className="content">
+                    <img src="https://cdn.discordapp.com/attachments/694815757665566741/713804082636390508/islam.png" />
+                    <Typing>
+                        <div className="quote-one">
+                            The noblest man is a human who can forgive the
+                            faults of others.
+                        </div>
+                        <div className="quote-two">
+                            The day of this glorious, I apologize for any
+                            mistakes.
+                        </div>
+                        <div className="quote-three">
+                            Purify our hearts purify ourselves.
+                        </div>
+                        <div className="quote-four">"Happy Eid Mubarak"</div>
+                        <div className="family">~Susetyo and Family</div>
+                    </Typing>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
